5573fe9639aa7a1ecbea7cf6f40bda526edb7dfb
ChristofferSAnkersen <chri5122@edu.eal.dk>
2018-11-12 09:50:13 +0100

    Models added

---
+++ CitiesAPI/Models/City.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CitiesAPI.Models
+{
+    public class City
+    {
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public List<Attraction> Attractions { get; set; }
+
+    }
+}
