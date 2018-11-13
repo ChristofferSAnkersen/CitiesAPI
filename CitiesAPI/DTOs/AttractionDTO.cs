5a9adc8975630bd6483d9f348245db4e73c4868a
ChristofferSAnkersen <chri5122@edu.eal.dk>
2018-11-13 13:48:38 +0100

    AttractionDTO classes made

--- CitiesAPI/DTOs/AttractionDTO.cs
+++ CitiesAPI/DTOs/AttractionDTO.cs
@@ -7,5 +7,10 @@ namespace CitiesAPI.DTOs
 {
     public class AttractionDTO
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
     }
 }
--- CitiesAPI/DTOs/AttractionsByCityDTO.cs
+++ CitiesAPI/DTOs/AttractionsByCityDTO.cs
@@ -7,5 +7,12 @@ namespace CitiesAPI.DTOs
 {
     public class AttractionsByCityDTO
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int CityId { get; set; }
     }
 }
